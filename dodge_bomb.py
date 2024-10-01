0bd98a462598922bf380cd60558aab31d2729b55
c0b23137 <c0b2313715@edu.teu.ac.jp>
2024-10-01 14:53:20 +0900

    練習３：爆弾反射開始

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,8 +1,8 @@
 import os
 import sys
+import random
 import pygame as pg
 
-import random
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {pg.K_UP:(0,-5),
@@ -10,6 +10,20 @@ DELTA = {pg.K_UP:(0,-5),
          pg.K_LEFT:(-5,0),
          pg.K_RIGHT:(+5,0),}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    引数：こうかとん、または、爆弾のRect
+    戻り値：真理値タプル（横判定結果、縦判定結果）
+    画面内ならTrue、画面外ならFalse
+    """
+    yoko,tate =True,True
+    if obj_rct.left <0 or WIDTH < obj_rct.right :
+        yoko = False
+    if obj_rct.top <0 or HEIGHT < obj_rct.bottom :
+        tate = False
+    return yoko,tate
 
 
 def main():
@@ -51,8 +65,15 @@ def main():
                 sum_mv[0] += tup[0]  # 横方向
                 sum_mv[1] += tup[1]  # 縦方向
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip((vx,vy))
+        yoko,tate = check_bound(bb_rct)
+        if not yoko:
+            vx*=-1
+        if not tate:
+            vy *=-1
         screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
