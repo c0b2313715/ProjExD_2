1ed91e10552e5c8fc978b1a98be28ef94bb3e817
c0b23137 <c0b2313715@edu.teu.ac.jp>
2024-10-01 14:30:29 +0900

    練習２：爆弾作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame as pg
 
+import random
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {pg.K_UP:(0,-5),
@@ -18,8 +19,17 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))
+    bb_img .set_colorkey((0,0,0))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_rct = bb_img.get_rect()
+#    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+    vx,vy= +5,+5
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -42,6 +52,8 @@ def main():
                 sum_mv[1] += tup[1]  # 縦方向
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip((vx,vy))
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
